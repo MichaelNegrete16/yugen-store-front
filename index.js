50ac5c607613aab34feeca3064c06d0d4a6df592
MichaelNegrete16 <michael.negrete16@gmail.com>
2026-07-09 22:47:50 -0500

    fix(store): polyfill react-native-get-random-values para cifrado (crypto-js)

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// Polyfill de crypto.getRandomValues (necesario para cifrar el estado
+// persistido con crypto-js). DEBE ir antes de importar la App.
+import 'react-native-get-random-values';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
